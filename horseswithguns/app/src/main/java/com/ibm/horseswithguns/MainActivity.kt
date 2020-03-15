db47f396375c22c77d3e019e5bad267fe3e4201c
Gurkirat Singh <gurkirat6060@gmail.com>
2020-03-15 17:49:41 +0530

    Final changes of the project horseswithguns

--- horseswithguns/app/src/main/java/com/ibm/horseswithguns/MainActivity.kt
+++ horseswithguns/app/src/main/java/com/ibm/horseswithguns/MainActivity.kt
@@ -2,11 +2,35 @@ package com.ibm.horseswithguns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+import android.content.Intent
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button1 = findViewById(R.id.icon1) as ImageView
+        button1.setOnClickListener{
+            val intent = Intent(this, airforce::class.java)
+
+            startActivity(intent)
+        }
+        val button2 = findViewById(R.id.icon2) as ImageView
+        button2.setOnClickListener{
+            val intent = Intent(this, army::class.java)
+
+            startActivity(intent)
+        }
+        val button3 = findViewById(R.id.icon3) as ImageView
+        button3.setOnClickListener{
+            val intent = Intent(this, navy::class.java)
+
+            startActivity(intent)
+        }
+
+
     }
 }
